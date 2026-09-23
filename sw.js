9877ca47c9898c0df964189b9e3159e83787ec26
ukuwa0122-star <u.kuwa0122@gmail.com>
2026-09-23 11:15:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chugaku-test-v5';
-const FILES=['./','./index.html','./style.css?v=2.5.0','./app.js?v=2.5.0','./questions.js?v=2.5.0','./challenge-questions.js?v=2.5.0','./version.json','./manifest.webmanifest','./icon.svg','./favicon.svg'];
+const CACHE='chugaku-test-v6';
+const FILES=['./','./index.html','./style.css?v=2.6.0','./app.js?v=2.6.0','./questions.js?v=2.6.0','./challenge-questions.js?v=2.6.0','./version.json','./manifest.webmanifest','./icon.svg','./favicon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
